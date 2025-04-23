fdfcfba44390af6c82408c6898d3a39e04157801
André Luiz Lessa <49814751+andrelessssa@users.noreply.github.com>
2025-04-23 10:55:00 -0300

    :rocket: basico
    :rocket: basico

--- codigo.py
+++ codigo.py
@@ -1,1 +1,11 @@
-pyautogui.hotkey("command", "space") 
+
+
+import pyautogui
+import time
+
+pyautogui.keyDown("command")
+time.sleep(0.1)  # Aguarde 100ms
+pyautogui.press("space")
+pyautogui.keyUp("command")
+pyautogui.write("chrome")
+pyautogui.press("enter")
